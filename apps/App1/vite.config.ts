f036370a416e1ff470fc59e201a2974fa55d528a
cdd <cdd@irm.kr>
2025-04-29 15:07:04 +0900

    feat: App1, App2 모듈 페더레이션 관련 vite.config.ts 커스텀 추가

--- apps/App1/vite.config.ts
+++ apps/App1/vite.config.ts
@@ -1,7 +1,36 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { federation } from "@module-federation/vite";
+import { dependencies } from "./package.json";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    federation({
+      name: "app1",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./app": "./src/App.tsx",
+      },
+      shared: {
+        react: {
+          requiredVersion: dependencies.react,
+          singleton: true,
+        },
+      },
+    }),
+  ],
+  build: {
+    target: "chrome89",
+  },
+  server: {
+    port: 4001,
+    fs: {
+      allow: [".", "../shared"],
+    },
+  },
+  preview: {
+    port: 4001,
+  },
 });
