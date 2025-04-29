18e2a2be87281ab71017f3804dd6fd2d27e07547
cdd <cdd@irm.kr>
2025-04-29 15:22:54 +0900

    feat: host 관련 federation 코드 추가

--- apps/host/vite.config.ts
+++ apps/host/vite.config.ts
@@ -1,7 +1,47 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { federation } from "@module-federation/vite";
+import { dependencies } from "./package.json";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    port: 4000,
+    fs: {
+      allow: [".", "../shared"],
+    },
+  },
+  preview: {
+    port: 4000,
+  },
+  build: {
+    target: "chrome89",
+  },
+  plugins: [
+    react(),
+    federation({
+      name: "host",
+      remotes: {
+        app1: {
+          type: "module",
+          name: "app1",
+          entry: "http://localhost:4001/remoteEntry.js",
+          entryGlobalName: "app1",
+          shareScope: "default",
+        },
+        app2: {
+          type: "module",
+          name: "app2",
+          entry: "http://localhost:4002/remoteEntry.js",
+          entryGlobalName: "app2",
+          shareScope: "default",
+        },
+      },
+      shared: {
+        react: {
+          requiredVersion: dependencies.react,
+          singleton: true,
+        },
+      },
+    }),
+  ],
+});
